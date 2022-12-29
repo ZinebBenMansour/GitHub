a0fcc7467cfa0659404c91706c604797c5629ebd
Zineb BenMansour <zineb9406@leedsenglish.com>
2022-12-29 00:06:32 +0000

    Overriding method parents class

---
+++ src/day_3/Practice.java
@@ -0,0 +1,22 @@
+package day_3;
+
+public class Practice {
+
+    /*
+    1. Create a class named Browser:
+
+			Methods:
+				openBrowser(): prints "opening chrome browser"
+				closeBrowser():  prints "closing chrome browser"
+     */
+
+    public String openBrowser(){
+        return "";
+    }
+    public String closeBrowser(){
+        return "";
+    }
+
+
+}
+
