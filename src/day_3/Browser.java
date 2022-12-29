5a0d842f4062a90763037f2e8351e53cf7ad2f2b
Zineb BenMansour <zineb9406@leedsenglish.com>
2022-12-29 00:22:38 +0000

    I made some changes

---
+++ src/day_3/Browser.java
@@ -0,0 +1,22 @@
+package day_3;
+
+public class Browser {
+
+    /*
+    1. Create a class named Browser:
+
+			Methods:
+				openBrowser(): prints "opening chrome browser"
+				closeBrowser():  prints "closing chrome browser"
+     */
+    public void openBrowser(){
+        System.out.println("Opening chrome browser");
+    }
+
+    public void closeBrowser(){
+        System.out.println("Closing chrome browser");
+    }
+
+}
+
+
